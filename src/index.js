35285ba91694c89234068a4f0571a2523bd099e6
Albert Nikulshin <gotodno@gmail.com>
2018-06-19 09:42:33 +0300

    adds eslint configuration

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 export const greetUser = () => console.log('Welcome to the Brain Games!\n');
 
 export const askForName = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-}
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
