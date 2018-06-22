d66824831d89d26996c7bbda09a7bbb6dca184a8
Albert Nikulshin <gotodno@gmail.com>
2018-06-22 20:14:48 +0300

    implementation of brain-balance game

---
+++ src/games/balance.js
@@ -0,0 +1,36 @@
+import { cons } from 'hexlet-pairs';
+import runGame from '..';
+import getRandomInteger from '../utils/get-random-integer';
+
+const minRandomInteger = 10;
+const maxRandomInteger = 6000;
+
+const balance = (num) => {
+  const iter = (digits) => {
+    const firstDigit = digits[0];
+    const lastDigit = digits[digits.length - 1];
+    if (lastDigit === (firstDigit + 1)) {
+      return Number.parseInt(digits.join(''), 10);
+    }
+    const newDigits = digits.map((d, idx) => {
+      if (idx === 0) return d + 1;
+      if (idx === (digits.length - 1)) return d - 1;
+      return d;
+    }).sort();
+    return iter(newDigits);
+  };
+  return iter(`${num}`.split('').map(d => Number.parseInt(d, 10)).sort());
+};
+
+export default () => {
+  const getGameData = () => {
+    const num = getRandomInteger(minRandomInteger, maxRandomInteger);
+    const question = `${num}`;
+    const answer = `${balance(num)}`;
+    return cons(question, answer);
+  };
+  runGame({
+    instruction: 'Balance the given number.',
+    getGameData,
+  });
+};
--- src/games/index.js
+++ src/games/index.js
@@ -1,3 +1,4 @@
 export { default as calc } from './calc';
 export { default as even } from './even';
 export { default as gcd } from './gcd';
+export { default as balance } from './balance';
